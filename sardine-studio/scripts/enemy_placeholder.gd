e9376baf620080464ed551423882e18367d5e735
asdJT <123706939+asdJT@users.noreply.github.com>
2026-02-18 13:01:16 -0800

    Enemy AI

--- sardine-studio/scripts/enemy_placeholder.gd
+++ sardine-studio/scripts/enemy_placeholder.gd
@@ -4,17 +4,28 @@ class_name Enemy extends CharacterBody2D
 const MAX_SPEED = 300.0
 const JUMP_VELOCITY = -700.0
 
+enum State {
+	FREE,
+	ATTACKING
+}
+var current_state = State.FREE
 
 @onready var progress_bar: ProgressBar = $enemybar
 @onready var hitbox: CollisionShape2D = $hitBox/CollisionShape2D
 var in_hit_box
 var player
 
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	match current_state:
+		State.FREE:
+			moving(delta)
+	
 
+func moving(delta):
 	# =Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction_x : float = -1 if (global_position.x > player.global_position.x) else 1
@@ -23,13 +34,21 @@ func _physics_process(delta: float) -> void:
 	distance_x -= 180
 	
 	var speed : float = MAX_SPEED if distance_x > 100 else MAX_SPEED * distance_x / 100
-	velocity.x = (direction_x * speed)
+	velocity.x = (direction_x * speed * delta)
 	
 	if is_on_floor() and (player.position.y - position.y < -50.0):
 		velocity.y = JUMP_VELOCITY
-
-	move_and_slide()
 	
+	move_and_slide()
+
+func attack():
+	animation_player
+
+func change_state(new_state):
+		current_state = new_state
+			
+
+
 
 func _ready() -> void:
 	progress_bar.value = 100
