e2c22a3fecf74a0b47ed6a22d8ee6a1b3eebe548
asdJT <JaTang10@s.sfusd.edu>
2026-03-07 18:30:34 -0800

    Fixed a bug

--- sardine-studio/rhythm.gd
+++ sardine-studio/rhythm.gd
@@ -129,11 +129,8 @@ func _process(delta: float) -> void:
 			print("prefect: " + str(prefect_count))
 			print("missed: " + str(missed_count))
 			print("Max Combo: " + str(max_combo))
-			note_progress["Next"] == "Empty"
-			await get_tree().create_timer(1.0).timeout
 			Rhythm_Game_Ended.emit()
-		elif note_progress["Next"] == "Empty":
-			pass
+
 
 func miss_check(note) -> void:
 	if note == null:
