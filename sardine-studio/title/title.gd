027fbf2dba0df84bc705b7f642c247d322de3f4d
asdJT <123706939+asdJT@users.noreply.github.com>
2026-04-08 13:15:58 -0700

    usable title screen

--- sardine-studio/title/title.gd
+++ sardine-studio/title/title.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+@onready var start_button: Button = %StartButton
+@onready var quit_button: Button = %QuitButton
+
+func _ready() -> void:
+	start_button.pressed.connect(start)
+	quit_button.pressed.connect(quit)
+	
+func start():
+	NavigationManager.go_to_level("garage", null)
+	
+func quit():
+	get_tree().quit()
