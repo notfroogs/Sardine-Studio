08deff5921b0f3659b429ed50edb7f0b03940381
asdJT <123706939+asdJT@users.noreply.github.com>
2026-05-08 15:46:59 -0700

    remove shaders

--- sardine-studio/pauseScreen/pause_screen.gd
+++ sardine-studio/pauseScreen/pause_screen.gd
@@ -6,17 +6,17 @@ extends Control
 @onready var quit_button: Button = %QuitButton
 @onready var help_button: Button = %HelpButton
 
-@export_range(0, 1.0) var menu_opened_amount := 0.0:
-	set = set_menu_opened_amount
-
-func set_menu_opened_amount(amount: float) -> void:
-	menu_opened_amount = amount
-	visible = amount > 0
-	if _ui_panel_container == null or _blur_color_rect == null:
-		return
-	_blur_color_rect.material.set_shader_parameter("blur_amount", lerp(0.0, 1.5, amount))
-	_blur_color_rect.material.set_shader_parameter("saturation", lerp(1.0, 0.3, amount))
-	_ui_panel_container.modulate.a = amount
+#@export_range(0, 1.0) var menu_opened_amount := 0.0:
+	#set = set_menu_opened_amount
+#
+#func set_menu_opened_amount(amount: float) -> void:
+	#menu_opened_amount = amount
+	#visible = amount > 0
+	#if _ui_panel_container == null or _blur_color_rect == null:
+		#return
+	#_blur_color_rect.material.set_shader_parameter("blur_amount", lerp(0.0, 1.5, amount))
+	#_blur_color_rect.material.set_shader_parameter("saturation", lerp(1.0, 0.3, amount))
+	#_ui_panel_container.modulate.a = amount
 
 func _ready() -> void:
 	pass
