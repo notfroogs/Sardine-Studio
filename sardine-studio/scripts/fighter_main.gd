edc7a27771c83e58f38c9c619f2c335c0e31ccc5
notfroogs <Jehuang@s.sfusd.edu>
2026-05-20 17:40:40 -0700

    add audio

--- sardine-studio/scripts/fighter_main.gd
+++ sardine-studio/scripts/fighter_main.gd
@@ -5,6 +5,10 @@ extends Node2D
 @onready var count_down: CountDown = $CanvasLayer/CountDown
 @onready var main_ui: Control = %"main ui"
 
+@onready var round_1: AudioStreamPlayer2D = $fighterPlayer/round1
+@onready var round_2: AudioStreamPlayer2D = $fighterPlayer/round2
+@onready var round_3: AudioStreamPlayer2D = $fighterPlayer/round3
+@onready var round_4: AudioStreamPlayer2D = $fighterPlayer/round4
 
 func _on_fighter_player_player_hit() -> void:
 	#if fighter_player(_on_area_2d_body_entered == true):
@@ -28,3 +32,11 @@ func _ready() -> void:
 			enemy_placeholder.sprite_2d.play("idle")
 	)
 	
+	if Gamemanager.enemy == "Ms_E":
+		round_4.play()
+	elif Gamemanager.enemy == "Boss":
+		round_3.play()
+	elif Gamemanager.enemy == "Mr_C":
+		round_2.play()
+	else:
+		round_1.play()
